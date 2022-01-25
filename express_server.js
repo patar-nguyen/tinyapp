8fadfdbb0c23a6d468d58f0c15f9d780fc7b8e2a
Patrick Nguyen <patar.nguyen@gmail.com>
2022-01-24 19:56:08 -0500

    Tiny App

--- express_server.js
+++ express_server.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const app = express();
 const PORT = 8080;
+app.set("view engine", "ejs");
+
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
-app.set("view engine", "ejs");
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
-
 
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
@@ -18,11 +18,6 @@ app.get("/urls/new", (req, res) => {
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
-});
-
-app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
-  res.render("urls_show", templateVars);
 });
 
 app.get("/", (req, res) => {
@@ -41,11 +36,25 @@ app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
 });
 
+function generateRandomString() {
+  let newURL = Math.random().toString(36).substr(2,6);
+  return newURL;
+}
+
+const randomKey = generateRandomString();
+
 app.post("/urls", (req, res) => {
   console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  res.redirect(`/urls/${randomKey}`);         // Respond with 'Ok' (we will replace this)
+  urlDatabase[randomKey] = req.body.longURL;
 });
 
-function generateRandomString() {
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
+  res.render("urls_show", templateVars);
+});
 
-}
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+});
